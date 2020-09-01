1154a8eef4a20079d7cbdd45bbe281208484e1ea
Davide Preti <davide.preti86@gmail.com>
2020-09-01 15:42:19 +0200

    Aggiunta nuova versione

--- js/main.js
+++ js/main.js
@@ -1,16 +1,40 @@
-const hamburger = $(".fas.fa-bars");
-const xClose = $(".fas.fa-times");
+// const hamburger = $(".fas.fa-bars");
+// const xClose = $(".fas.fa-times");
 
 
-hamburger.click(
-    function () {
-        $(".hamburger-menu").show();
-    }
-)
+// hamburger.click(
+//     function () {
+//         $(".hamburger-menu").show();
+//     }
+// )
 
-xClose.click(
-    function () {
-        $(".hamburger-menu").hide();
-    }
+// xClose.click(
+//     function () {
+//         $(".hamburger-menu").hide();
+//     }
+// )
 
-)
+
+// nuova versione
+const hamburger = $("a > i.fas.fa-bars");
+const xClose = $("a > i.fas.fa-times");
+
+$(document).ready(function () {
+
+    // al click sull'icona hamburger aggiungo la classe "active" a div.hamburger-menu
+    hamburger.click(
+        function () {
+            $(".hamburger-menu").addClass("active");
+        }
+    )
+
+    // al click sull'icona x rimuovo la classe "active" a div.hamburger-menu
+    xClose.click(
+        function () {
+            $(".hamburger-menu").removeClass("active");
+        }
+    )
+
+});
+
+
